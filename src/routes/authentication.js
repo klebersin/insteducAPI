f524c5b545fc535dec9bcdb4b6569264d453823c
klebersin <kleber.sik@gmail.com>
2022-06-20 20:09:01 -0500

    first commit

---
+++ src/routes/authentication.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+const cors = require('cors')
+
+router.get('/', (req, res) => {
+    res.send('Hello')
+})
+
+router.post('/', cors(), (req, res) => {
+    console.log(req.body)
+    if (req.body.user === 'admin' && req.body.password === '12345') {
+        const payload = {
+            check: true
+        }
+        const token = jwt.sign(payload, 'claveJWT', {
+            expiresIn: '7d'
+        })
+        res.json({
+            message: 'Succesfully',
+            token: token
+        })
+    } else {
+        const payload = {
+            check: true
+        }
+        const token = jwt.sign(payload, 'claveJWT', {
+            expiresIn: '7d'
+        })
+        res.json({
+            message: 'Succesfully',
+            token: token
+        })
+    }
+})
+
+module.exports = router;
